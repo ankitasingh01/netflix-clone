0c2ee67a4d2b1f732f58e14d3912a932fb58e7ff
ankitasingh01 <er.ankita9@gmail.com>
2022-03-03 16:34:11 +0530

    March 3 code navbar added

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <img
+            className='nav__logo'
+        />
+    </div>
+  )
+}
+
+export default Nav
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     // A snippet of code which runs based on specific condition/variabele
@@ -20,7 +20,7 @@ function Row({title, fetchUrl}) {
         fetchData();
     },[fetchUrl]);
 
-    console.table(movies);    
+    //console.table(movies);    
 
   return (
     <div className='row'>
@@ -30,8 +30,9 @@ function Row({title, fetchUrl}) {
             {movies.map(movie => (
                 <img 
                     key={movie.id}
-                    className='row__poster'
-                    src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    src={`${base_url}${
+                        isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
             ))}
         </div>
         {/* container->posters */}        
